7a8ab37d324b762310fa49932efb604c205e289b
CS++ <kevin@csplusplus.com>
2025-02-04 11:35:30 -0800

    Update ReviewTest.java

--- src/test/java/cr/ReviewTest.java
+++ src/test/java/cr/ReviewTest.java
@@ -1,9 +1,9 @@
 package cr;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.io.File;
 import java.io.IOException;
